5d3af2bb211506b352109ccf9da5bf17768a8dee
Abhishek Sharma <sharma312006@gmail.com>
2024-05-16 23:26:30 +0530

    fix webhooks issue

--- src/app/api/webbhooks/route.ts
+++ src/app/api/webbhooks/route.ts
@@ -7,6 +7,8 @@ import Stripe from "stripe";
 export async function POST(req: Request) {
   try {
     const body = await req.text();
+    console.log(body);
+
     const signature = headers().get("stripe-signature");
     if (!signature) {
       return new Response("Invalid signature", { status: 400 });
